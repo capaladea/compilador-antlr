07626488a0d9497c847148c7aefe6d1930ced849
Cristian Paladea <albertopaladea@gmail.com>
2026-06-15 23:35:26 -0300

    Cambios menores en Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,19 @@ public class Main {
         // Tu código de prueba respetando la gramática que diseñaste
         String input = "src/test/prueba01.prg";
 
-        // 1. Leer el código fuente e iniciar el Lexer
+        // Leer la ruta del archivo fuente e iniciar el Lexer
         GramaticaLexer lexer = new GramaticaLexer(CharStreams.fromFileName(input));
 
-        // 2. Generar el flujo de tokens
+        // Generar el flujo de tokens
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        // 3. Pasarle los tokens al Parser
+        // Pasarle los tokens al Parser
         GramaticaParser parser = new GramaticaParser(tokens);
 
         System.out.println("--- Ejecutando el Compilador ---");
 
-        // 4. Llamar a la regla raíz 'program'.
-        // Esto va a parsear, armar el AST y ejecutar el 'n.execute()' de cada nodo.
+        // Llamar a la regla raíz 'program'.
+        // Parsear y armar el AST, y ejecutar el 'n.execute()' de cada nodo.
         parser.program();
 
         System.out.println("--------------------------------");
